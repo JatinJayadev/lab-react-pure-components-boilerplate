5f54020aec06426a47c99c086017ecb0da844eff
Jatin Jayadev <jatin.batchu@kalvium.community>
2024-02-05 16:53:26 +0530

    Adding

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import RegularComponents from './Components/RegularComponents'
+import PureComponent from './Components/PureComponents'
+
+function App() {
+
+  return (
+    <>
+      <RegularComponents />
+      <PureComponent />
+    </>
+  )
+}
+
+export default App
